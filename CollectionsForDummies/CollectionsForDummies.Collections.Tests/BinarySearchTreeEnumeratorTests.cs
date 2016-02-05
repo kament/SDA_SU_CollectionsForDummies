22f98aaa9f620c9858b2d85f5a2925e4c713bccb
kament <kamen_t@abv.bg>
2016-02-05 16:42:39 +0200

    Test enumerators

---
+++ CollectionsForDummies/CollectionsForDummies.Collections.Tests/BinarySearchTreeEnumeratorTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CollectionsForDummies.Collections.Tests
+{
+    [TestClass]
+    public class BinarySearchTreeEnumeratorTests
+    {
+        [TestMethod]
+        public void ShouldEnumerateCorrectlyEnumeratePreOrderTreeWalk()
+        {
+            var avlTree = new AvlTree<int>(new int[] { 30, 15, 14, 18, 34, 44, 33 });
+
+            avlTree.TreeWalk = TreeWalk.PreOrder;
+
+            var resultFromTravers = string.Empty;
+
+            foreach (var item in avlTree)
+            {
+                resultFromTravers += " " + item.Value.ToString();
+            }
+
+            var expectedTravers = "30 15 14 18 34 33 44";
+
+            Assert.AreEqual(expectedTravers, resultFromTravers.Trim());
+        }
+
+        [TestMethod]
+        public void ShouldEnumerateCorrectlyEnumerateInOrderTreeWalk()
+        {
+            var avlTree = new AvlTree<int>(new int[] { 30, 15, 14, 18, 34, 44, 33 });
+
+            avlTree.TreeWalk = TreeWalk.InOrder;
+
+            var resultFromTravers = string.Empty;
+
+            foreach (var item in avlTree)
+            {
+                resultFromTravers += " " + item.Value.ToString();
+            }
+
+            var expectedTravers = "14 15 18 30 33 34 44";
+
+            Assert.AreEqual(expectedTravers, resultFromTravers.Trim());
+        }
+
+        [TestMethod]
+        public void ShouldEnumerateCorrectlyEnumeratePostOrderTreeWalk()
+        {
+            var avlTree = new AvlTree<int>(new int[] { 30, 15, 14, 18, 34, 44, 33 });
+
+            avlTree.TreeWalk = TreeWalk.PostOrder;
+
+            var resultFromTravers = string.Empty;
+
+            foreach (var item in avlTree)
+            {
+                resultFromTravers += " " + item.Value.ToString();
+            }
+
+            var expectedTravers = "14 18 15 33 44 34 30";
+
+            Assert.AreEqual(expectedTravers, resultFromTravers.Trim());
+        }
+    }
+}
